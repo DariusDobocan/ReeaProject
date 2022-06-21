c41c4bb4911eec8d731aeeb21ec7d89407e41019
dobo <dobocan.darius2002@gmail.com>
2022-06-21 13:12:33 +0300

    new commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//this is keys
+if(process.env.NODE_ENV === 'production'){
+  //we are in production
+  module.exports = require('./prod');
+} else {
+  //we are in developement
+  module.exports = require('./dev');
+}
