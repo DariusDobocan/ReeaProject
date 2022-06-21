58309132a219c4500a25c8252de4553dad4ff84e
dobo <dobocan.darius2002@gmail.com>
2022-06-21 13:03:45 +0300

    new commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//this is dev
+module.exports = {
+  googleClientID: '420542905377-5s4oolk4uo8p5kf3iq4k054elmkcf6i3.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  mongoURI: 'mongodb+srv://Morofilu:Grd3g8329uh0XL7H@cluster0.chvjqr6.mongodb.net/emailydb?retryWrites=true&w=majority',
+  cookieKey: 'oidvbidvbsifdvndsijvbkjjkndfvddjnfji3727'
+
+};
--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passports');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(session({
+   secret: 'somethingsecretgoeshere',
+   resave: false,
+   saveUninitialized: true,
+   cookieSession: { secure: true }
+}));
+
+//app.use(
+//  cookieSession({
+  //  maxAge: 30 * 24 *60 * 60 * 1000,
+    //keys: [keys.cookieKey]
+  //})
+//);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy'});
-});
-
 app.listen(PORT);
